275f82166ad7cc9bba16b9d8cc41635375259546
Claire Cassidy <clacassidy@gmail.com>
2019-06-24 20:40:48 -0700

    worked on filter

--- filter.js
+++ filter.js
@@ -1,3 +1,10 @@
 const array = [1, 2,, 4, 5];
 
+const
+
 function filter(arr)
+  const filtered = [];
+  for(let i = 0; i < arr.length; i++) {
+    filtered[i] = callback(arr[i]);
+}
+return filtered;
--- map.js
+++ map.js
@@ -1,5 +1,9 @@
 const array = [1, 2, , 4, 5];
 
+const addTwo = map(array, array => array + 2);
+
+console.log(addTwo);
+
 function map(arr, callback) {
     const mapped = [];
     for(let i = 0; i < arr.length; i++) {
