01f10f855c66b71844a727571ad55d904d10de28
Claire Cassidy <clacassidy@gmail.com>
2019-06-30 14:20:20 -0700

    findIndex tests and function

--- function.js
+++ function.js
@@ -1,34 +1,34 @@
 function map(arr, callback) {
-    const mapped = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(!arr.hasOwnProperty(i)) continue;
+  const mapped = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(!arr.hasOwnProperty(i)) continue;
 
-        mapped[i] = callback(arr[i]);
-    }
-    return mapped;
+    mapped[i] = callback(arr[i]);
+  }
+  return mapped;
 }
 
 function filter(arr, callback) {
-    const filtered = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(!ar.hasOwnProperty(i)) continue;
+  const filtered = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(!arr.hasOwnProperty(i)) continue;
 
-        if(callback(arr[i])) {
-            filtered[filtered.length] = arr[i];
-        }
+    if(callback(arr[i])) {
+      filtered[filtered.length] = arr[i];
     }
-    return filtered;
+  }
+  return filtered;
 }
 
 function findIndex(arr, callback) {
-    for(let i = 0; i < arr.length; i++) {
-        if(arr.hasOwnProperty(i) && callback(arr[i])) return i;
-    }
-    return i;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr.hasOwnProperty(i) && callback(arr[i])) return i;
+  }
+  return i;
 }
 
 module.exports = {
-    map,
-    filter,
-    findIndex
+  map,
+  filter,
+  findIndex
 };
--- function.test.js
+++ function.test.js
@@ -1,108 +1,120 @@
-const { 
-    map, 
-    filter,
-    findIndex
-} = require('./function.js');
-
+const {
+  map,
+  filter,
+  findIndex,
+} = require('./index');
+  
 describe('array methods', () => {
-    describe('map functionality', () => {
-        it('returns an array', () => {
-            const numbers = [1, 2, 3];
-            const mapped = map(numbers, number => number * 2);
+  describe('map function', () => {
+    it('returns an array', () => {
+      const numbers = [1, 2, 3];
+      const mapped = map(numbers, number => number * 2);
+  
+      expect(mapped).toEqual(expect.any(Array));
+    });
+  
+    it('returns a mapped array with the same length as arr', () => {
+      const colors = ['red', 'green', 'blue', 'yellow'];
+      const mapped = map(colors, color => color.toUpperCase());
+  
+      // expect(mapped.length).toEqual(colors.length);
+      expect(mapped).toHaveLength(colors.length);
+    });
+  
+    it('returns a mapped array with uppercase colors', () => {
+      const colors = ['red', 'green', 'blue', 'yellow'];
+      const mapped = map(colors, color => color.toUpperCase());
+  
+      expect(mapped).toEqual([
+        'RED',
+        'GREEN',
+        'BLUE',
+        'YELLOW'
+      ]);
+    });
+  
+    it('returns a mapped array with doubled numbers', () => {
+      const numbers = [1, 2, 3, 4];
+      const mapped = map(numbers, number => number * 2);
+  
+      expect(mapped).toEqual([
+        2,
+        4,
+        6,
+        8
+      ]);
+    });
+  
+    it('skips holes', () => {
+      // eslint-disable-next-line no-sparse-arrays
+      const numbers = [1, , 3, 4];
+      const callback = jest.fn();
+      map(numbers, callback);
+  
+      // 3 times since the hole is skipped
+      expect(callback).toHaveBeenCalledTimes(3);
+    });
+  });
+  
+  describe('filter function', () => {
+    it('returns an array', () => {
+      const numbers = [1, 2, 3];
+      const filtered = filter(
+        numbers,
+        number => number % 2 === 0
+      );
+  
+      expect(filtered).toEqual(expect.any(Array));
+    });
+  
+    it('filters out odd numbers', () => {
+      const numbers = [1, 2, 3, 4];
+      const evens = filter(
+        numbers,
+        number => number % 2 === 0
+      );
+  
+      expect(evens).toEqual([2, 4]);
+    });
+  
+    it('skips holes', () => {
+      // eslint-disable-next-line no-sparse-arrays
+      const numbers = [1, , 3, 4];
+      const callback = jest.fn();
+      filter(numbers, callback);
+  
+      expect(callback).toHaveBeenCalledTimes(3);
+    });
+  });
 
-            expect(mapped).toEqual(expect.any(Array));
-        });
+  describe('findIndex function', () => {
+    it('returns a number', () => {
+      const colors = ['red', 'blue', 'green'];
+      const index = findIndex(colors, color => color.includes('l'));
 
-        it('returns a mapped array with the same length as arr', () => {
-            const colors = ['red', 'green', 'blue', 'yellow'];
-            const mapped = map(colors, color => color.toUpperCase());
-
-            expect(mapped).toHaveLength(colors.length);
-        });
-
-        it('returns a mapped array with uppercase colors', () => {
-            const colors = ['red', 'green', 'blue', 'yellow'];
-            const mapped = map(colors, color => color.toUpperCase());
-
-            expect(mapped).toEqual([
-                'RED',
-                'GREEN',
-                'BLUE',
-                'YELLOW'
-            ]);
-        });
-
-        it('returns a mapped array with doubled numbers', () => {
-            const numbers = [1, 2, 3, 4];
-            const mapped = map(numbers, number => number * 2);
-
-            expect(mapped).toEqual([
-                2,
-                4,
-                6,
-                8
-            ]);
-        });
+      expect(index).toEqual(expect.any(Number));
     });
 
-    describe('filter functionality', () => {
-        it('returns an array', () => {
-            const numbers = [1, 2, 3];
-            const filtered = filter(
-                numbers,
-                number => number % 2 === 0
-            );
+    it('returns the index of a matching item', () => {
+      const colors = ['red', 'blue', 'green'];
+      const index = findIndex(colors, color => color.includes('l'));
 
-            expect(filtered).toEqual(expect.any(Array));
-        });
-        
-        it('filters out odd numbers', () => {
-            const numbers = [1, 2, 3, 4];
-            const evens = filter(
-                numbers,
-                number => number % 2 === 0
-            );
-            
-            expect(evens).toEqual([2, 4]);
-        });
+      expect(index).toEqual(1);
+    });
+    it('returns -1 if no match', () => {
+      const colors = ['red', 'blue', 'green'];
+      const index = findIndex(colors, color => color.includes('y'));
 
-        it('skips holes', () => {
-            const numbers = [1, , 3, 4];
-            const callback = jest.fn();
-            filter(numbers, callback);
-
-            expect(callback).toHaveBeenCalledTimes(3);
-        });
+      expect(index).toEqual(-1);
     });
 
-    describe('findIndex function', () => {
-        it('returns a number', () => {
-            const colors = ['red', 'blue', 'green'];
-            const index = findIndex(colors, color => color.includes('l'));
+    it('skips holes', () => {
+      // eslint-disable-next-line no-sparse-arrays
+      const numbers = [1, , 3, 4];
+      const callback = jest.fn();
+      findIndex(numbers, callback);
 
-            expect(index).toEqual(expect.any(Number));
-        });
-
-        it('returns the index of a matching item', () => {
-            const colors = ['red', 'blue', 'green'];
-            const index = findIndex(colors, color => color.includes('l'));
-
-            expect(index).toEqual(1);
-        });
-
-        it('returns -1 if no match', () => {
-            const colors = ['red', 'blue', 'green'];
-            const index = findIndex(colors, color => color.includes('y'));
-
-            expect(index).toEqual(-1);
-        });
-
-        it('skips holes', () => {
-            const numbers = [1, , 3, 4];
-            const callback = jest.fn();
-            findIndex(numbers, callback);
-
-            expect(callback).toHaveBeenCalledTimes(3);
-        });
+      expect(callback).toHaveBeenCalledTimes(3);
     });
+  });
 });
